b3341b1fad38a1c2eef0acd7d10d93896be29426
Ion Bold <bold.ion86@gmail.com>
2022-06-20 19:53:20 +0300

    Command send via messages class added, crc16 set and verify functions added.
    Signed-off-by: Ion Bold <bold.ion86@gmail.com>

--- include/messages.h
+++ include/messages.h
@@ -11,19 +11,26 @@
 #define PAYLOAD_SIZE    0x04    // 4 bytes
 #define CRC_SIZE        0x02    // 2 bytes
 
+typedef enum
+{
+    GET_TEMPERATURE     = ((uint8_t)0xA0),
+    GET_HUMIDITY        = ((uint8_t)0xA1)
+} Command_TypeDef;
+
+typedef enum
+{
+    ACK                 = ((uint8_t)0x0A), 
+    NACK                = ((uint8_t)0x0F)
+} Response_TypeDef;
+
 /* Exported classes ----------------------------------------------------------*/
 class Command
 {
     public:
-        typedef enum
-        {
-            GET_TEMPERATURE     = ((uint8_t)0xA0),
-            GET_HUMIDITY        = ((uint8_t)0xA1)
-        } Command_TypeDef;
-
         Command_TypeDef cmd;
         uint8_t destAddress;
         uint16_t crc;
+        uint8_t commandArray[COMMAND_SIZE];
 
         Command(uint8_t *data);
         Command(Command_TypeDef c, uint8_t address);
@@ -32,15 +39,10 @@ class Command
 class Response
 {
     public:
-        typedef enum
-        {
-            ACK                 = ((uint8_t)0x0A), 
-            NACK                = ((uint8_t)0x0F)
-        } Response_TypeDef;
-
         Response_TypeDef rsp;
         uint32_t payload;
         uint16_t crc;
+        uint8_t responseArray[RESPONSE_SIZE];
 
         Response(uint8_t *data);
         Response(Response_TypeDef r, uint32_t load);
--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,19 @@
 #include "messages.h"
 #include <RH_ASK.h>
 #ifdef RH_HAVE_HARDWARE_SPI
-#include <SPI.h> // Not actually used but needed to compile
+#include <SPI.h>                // Not actually used but needed to compile
 #endif
 
-RH_ASK driver(2000, 4, 5, 0); // ESP8266 or ESP32: do not use pin 11 or 2
+// Sensors destination addreses (more can be added)
+#define SENSOR1     0x01        // Sensor1 destination address
+#define SENSOR2     0x02        // Sensor2 destination address
+
+RH_ASK driver(2000, 4, 5, 0);   // ESP8266 or ESP32: do not use pin 11 or 2
 
 void setup()
 {
 #ifdef RH_HAVE_SERIAL
-    Serial.begin(115200);	  // Debugging only
+    Serial.begin(115200);	    // Debugging only
 #endif
     if (!driver.init())
 #ifdef RH_HAVE_SERIAL
@@ -22,9 +26,9 @@ void setup()
 
 void loop()
 {
-    const char *msg = "hello";
+    Command myCommand(GET_TEMPERATURE, SENSOR1);
 
-    driver.send((uint8_t *)msg, strlen(msg));
+    driver.send(myCommand.commandArray, COMMAND_SIZE);
     driver.waitPacketSent();
     delay(1000);
 }
