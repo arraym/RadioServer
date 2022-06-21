b349940e80060f2da07534c974577b106c191012
Ion Bold <bold.ion86@gmail.com>
2022-06-21 17:50:50 +0300

    Set CRC function bug solved.
    Signed-off-by: Ion Bold <bold.ion86@gmail.com>

--- src/messages.cpp
+++ src/messages.cpp
@@ -19,11 +19,11 @@ bool Verify_CRC(uint8_t *data, uint8_t size);
   */
 Command::Command(uint8_t *data)
 {
-   cmd = (Command_TypeDef)data[0];
-   destAddress = data[1];
-   crc = (uint16_t)(data[2] << 8) | data[3];
-   memcpy(commandArray, data, COMMAND_SIZE);
-   if(!Verify_CRC(data, COMMAND_SIZE))
+    cmd = (Command_TypeDef)data[0];
+    destAddress = data[1];
+    crc = (uint16_t)(data[2] << 8) | data[3];
+    memcpy(commandArray, data, COMMAND_SIZE);
+    if(!Verify_CRC(data, COMMAND_SIZE))
       cmd = (Command_TypeDef)0xFF;
 }
 
@@ -35,11 +35,11 @@ Command::Command(uint8_t *data)
   */
  Command::Command(Command_TypeDef c, uint8_t address)
  {
-   cmd = c;
-   destAddress = address;
-   commandArray[0] = c;
-   commandArray[1] = address;
-   crc = Set_CRC((uint8_t*)&commandArray, COMMAND_SIZE);
+    cmd = c;
+    destAddress = address;
+    commandArray[0] = c;
+    commandArray[1] = address;
+    crc = Set_CRC((uint8_t*)&commandArray, COMMAND_SIZE);
  }
 
 /**
@@ -49,11 +49,11 @@ Command::Command(uint8_t *data)
   */
  Response::Response(uint8_t *data)
  {
-   rsp = (Response_TypeDef)data[0];
-   memcpy((uint8_t*)&payload, (uint8_t*)&data[1], PAYLOAD_SIZE);
-   crc = (uint16_t)(data[5] << 8) | data[6];
-   memcpy(responseArray, data, RESPONSE_SIZE);
-   if(!Verify_CRC(data, RESPONSE_SIZE))
+    rsp = (Response_TypeDef)data[0];
+    memcpy((uint8_t*)&payload, (uint8_t*)&data[1], PAYLOAD_SIZE);
+    crc = (uint16_t)(data[5] << 8) | data[6];
+    memcpy(responseArray, data, RESPONSE_SIZE);
+    if(!Verify_CRC(data, RESPONSE_SIZE))
       rsp = NACK;
  }
 
@@ -65,11 +65,11 @@ Command::Command(uint8_t *data)
   */
  Response::Response(Response_TypeDef r, uint32_t load)
  {
-   rsp = r;
-   payload = load;
-   responseArray[0] = r;
-   memcpy((uint8_t*)&responseArray[1], (uint8_t*)load, PAYLOAD_SIZE);
-   crc = Set_CRC((uint8_t*)&responseArray, RESPONSE_SIZE);
+    rsp = r;
+    payload = load;
+    responseArray[0] = r;
+    memcpy((uint8_t*)&responseArray[1], (uint8_t*)&load, PAYLOAD_SIZE);  
+    crc = Set_CRC((uint8_t*)&responseArray, RESPONSE_SIZE);
  }
 
  /**
@@ -80,17 +80,17 @@ Command::Command(uint8_t *data)
   */
  uint16_t Set_CRC(uint8_t *data, uint8_t size)
  {
-   uint16_t crcValue = 0;
-   CRC16 crc;
+    uint16_t crcValue = 0;
+    CRC16 crc;
 
-   crc.setPolynome(0x1021);
-   crc.add(data, (uint16_t)(size - CRC_SIZE));
-   crcValue = crc.getCRC();
+    crc.setPolynome(0x1021);
+    crc.add(data, (uint16_t)(size - CRC_SIZE));
+    crcValue = crc.getCRC();
 
-   data[2] = crcValue >> 8;
-   data[3] = crcValue & 0xFF;
+    data[size - 2] = crcValue >> 8;
+    data[size - 1] = crcValue & 0xFF;
 
-   return crcValue;
+    return crcValue;
  }
 
   /**
@@ -101,16 +101,16 @@ Command::Command(uint8_t *data)
   */
  bool Verify_CRC(uint8_t *data, uint8_t size)
  {
-   uint16_t crcValue = 0;
-   uint16_t receivedCrc = (uint16_t)(data[size - 2] << 8) | data[size - 1];
-   CRC16 crc;
+    uint16_t crcValue = 0;
+    uint16_t receivedCrc = (uint16_t)(data[size - 2] << 8) | data[size - 1];
+    CRC16 crc;
 
-   crc.setPolynome(0x1021);
-   crc.add(data, (uint16_t)(size - CRC_SIZE));
-   crcValue = crc.getCRC();
+    crc.setPolynome(0x1021);
+    crc.add(data, (uint16_t)(size - CRC_SIZE));
+    crcValue = crc.getCRC();
 
-   if(crcValue != receivedCrc)
+    if(crcValue != receivedCrc)
       return false;
 
-   return true;
+    return true;
  }
